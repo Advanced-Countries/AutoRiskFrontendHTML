47e7600057b05e3b88fdb87bb2f84af01a9aa9b1
zjjppiscool <jpzen@outlook.com>
2017-12-28 20:20:17 -0600

    chat

--- autoriskbot.js
+++ autoriskbot.js
@@ -3,39 +3,39 @@
 	client.on('ready', () => {
 		console.log('[CLIENT] Ready!');
 		window.generalChannel = client.guilds.find('id', '386688984845123585').channels.find('name', 'spam'); //Find the #spam channel, and set it to var generalChannel.
-		window.botDm = client.guilds.find('id', '386688984845123585').members.find("id", "386688418224275456"); //Find The Cartographer's user object, and save to botDm.
+		window.generalChannel = client.guilds.find('id', '386688984845123585').members.find("id", "386688418224275456"); //Find The Cartographer's user object, and save to generalChannel.
 		window.myId = client.user.id;
-		generalChannel.send("!fullmap");//Gets the map, list of countries, and guns available.
+		generalChannel.send("!fullmap"); //Gets the map, list of countries, and guns available.
 		generalChannel.send("!list")
 		generalChannel.send("!guns");
 		generalChannel.send("!stats");
 		generalChannel.send("!force webInt");
 		generalChannel.send("!resource webInt");
 	});
-	client.on('debug', console.log);//Logs what the bot is doing.
+	client.on('debug', console.log); //Logs what the bot is doing.
 	client.on('error', console.error);
-	client.ws.on('close', (event) => console.log('[CLIENT] Disconnect!', event));//Logs out.
-	client.on('message', (message) => {//When it detects a message.
-		if (message.author.id === "386688418224275456") {//Only check messages if the author is the cartographer.
-			console.log(message.content);//Logs the message for simple debugging.
-			if (message.content === "Fullmap:") {//If the message is blank, it must have the map.
-				document.getElementById("map").src = message.attachments.first().url;//Grab the url of the map, and make it the src of the map image.
-			} else if (message.channel.id === "386688984845123587") {//If it is in the daily news chanenl, it is a daily news. So, put it in the daily news area.
+	client.ws.on('close', (event) => console.log('[CLIENT] Disconnect!', event)); //Logs out.
+	client.on('message', (message) => { //When it detects a message.
+		if (message.author.id === "386688418224275456") { //Only check messages if the author is the cartographer.
+			console.log(message.content); //Logs the message for simple debugging.
+			if (message.content === "Fullmap:") { //If the message is blank, it must have the map.
+				document.getElementById("map").src = message.attachments.first().url; //Grab the url of the map, and make it the src of the map image.
+			} else if (message.channel.id === "386688984845123587") { //If it is in the daily news chanenl, it is a daily news. So, put it in the daily news area.
 				document.getElementById("news").innerHTML = "Last Daily News:\n" + message.content;
 				generalChannel.send("!fullmap");
 				generalChannel.send("!list");
 				generalChannel.send("!stats");
 				generalChannel.send("!force webInt");
 				generalChannel.send("!resource webInt");
-			} else if (message.content.includes("guns")) {//If it includes the word guns, it is the gun list. Put it in the guns area.
+			} else if (message.content.includes("guns")) { //If it includes the word guns, it is the gun list. Put it in the guns area.
 				document.getElementById("guns").innerHTML = message.content;
-			} else if (message.content.includes(`${myId}WEBINT_FORCE`)) {//If it contains force and your id, it is your force. Put it in the force area.
+			} else if (message.content.includes(`${myId}WEBINT_FORCE`)) { //If it contains force and your id, it is your force. Put it in the force area.
 				document.getElementById("force").innerHTML = message.content.replace(`${myId}WEBINT_FORCE`, "");
-			} else if (message.content.includes(`${myId}WEBINT_RESOURCE`)) {//If it contains "you mine" and your mention, it is your resources. Put in in the resources ara.
+			} else if (message.content.includes(`${myId}WEBINT_RESOURCE`)) { //If it contains "you mine" and your mention, it is your resources. Put in in the resources ara.
 				document.getElementById("resource").innerHTML = message.content.replace(`${myId}WEBINT_RESOURCE`, "");
-			} else if (message.content.includes(client.user.tag)) {//If it contains your tag, ("<username><#1234>") it is your stats. Put it in the stats area.
+			} else if (message.content.includes(client.user.tag)) { //If it contains your tag, ("<username><#1234>") it is your stats. Put it in the stats area.
 				document.getElementById("stats").innerHTML = "Your Stats:\n" + message.content;
-			} else if (message.content.includes("List of countries:")){
+			} else if (message.content.includes("List of countries:")) {
 				document.getElementById("list").innerHTML = message.content;
 			}
 		}
@@ -48,9 +48,10 @@
 		generalChannel.send("!war " + where);
 		setTimeout(function () {
 			generalChannel.send("!fullmap");
-		}, 1000);//Waits 1 second, then updates the map.
+		}, 1000); //Waits 1 second, then updates the map.
 	}
-	function update(){
+
+	function update() {
 		setTimeout(function () {
 			generalChannel.send("!stats " + client.user.toString());
 			generalChannel.send("!resource " + client.user.toString());
@@ -58,16 +59,16 @@
 		}, 1000);
 	}
 	document.getElementById("warall").onclick = function () {
-		war("all");//Most of these are self explanatory, this one wars all.
+		war("all"); //Most of these are self explanatory, this one wars all.
 	};
 	document.getElementById("warnone").onclick = function () {
 		war("none");
 	};
 	document.getElementById("warcountry").onclick = function () {
-		war(prompt("Which country would you like to war? None to cancel", "Country"));//Prompts for country to war.
+		war(prompt("Which country would you like to war? None to cancel", "Country")); //Prompts for country to war.
 	};
 	document.getElementById("setpower").onclick = function () {
-		generalChannel.send("!manpower " + prompt("What manpower would you like? (1-100)", "Numbers Only"));//Prompts for manpower to use.
+		generalChannel.send("!manpower " + prompt("What manpower would you like? (1-100)", "Numbers Only")); //Prompts for manpower to use.
 		update();
 	};
 	document.getElementById("setgun").onclick = function () {
@@ -97,10 +98,52 @@
 		update();
 	};
 	document.getElementById("map").onclick = function () {
-		botDm.send("!fullmap");
-		botDm.send("!list");
-		generalChannel.send("!stats " + client.user.toString());
-		generalChannel.send("!force " + client.user.toString());
-		generalChannel.send("!resource " + client.user.toString());
+		generalChannel.send("!fullmap");
+		generalChannel.send("!list");
+		generalChannel.send("!stats webInt");
+		generalChannel.send("!force webInt");
+		generalChannel.send("!resource webInt");
+	}
+	document.getElementById("upd8").onclick = function () {
+		var tmp = "";
+		var last5 = client.guilds.find('id', '386688984845123585').channels.find('name', 'general').messages.last(5);
+		for (msg in last5) {
+			tmp += `[${msg.author.tag}] ${msg.author.content}\n`;
+		}
+		document.getElementById("chatbox").innerHTML = tmp;
+	}
+	document.getElementById("upd81").onclick = function () {
+		var tmp = "";
+		var last5 = client.guilds.find('id', '386688984845123585').channels.find('name', 'spam').messages.last(5);
+		for (msg in last5) {
+			tmp += `[${msg.author.tag}] ${msg.author.content}\n`;
+		}
+		document.getElementById("spambox").innerHTML = tmp;
+	}
+
+	document.getElementById("sendBtn").onclick = function () {
+		var sendTo = client.guilds.find('id', '386688984845123585').channels.find('name', 'general');
+		sendTo.send(document.getElementById("chatTxt").value);
+		setTimeout(function () {
+			var tmp = "";
+			var last5 = client.guilds.find('id', '386688984845123585').channels.find('name', 'general').messages.last(5);
+			for (msg in last5) {
+				tmp += `[${msg.author.tag}] ${msg.author.content}\n`;
+			}
+			document.getElementById("chatbox").innerHTML = tmp;
+		}, 100);
+	}
+
+	document.getElementById("sendBtn1").onclick = function () {
+		var sendTo = client.guilds.find('id', '386688984845123585').channels.find('name', 'spam');
+		sendTo.send(document.getElementById("chatTxt1").value);
+		setTimeout(function () {
+			var tmp = "";
+			var last5 = client.guilds.find('id', '386688984845123585').channels.find('name', 'spam').messages.last(5);
+			for (msg in last5) {
+				tmp += `[${msg.author.tag}] ${msg.author.content}\n`;
+			}
+			document.getElementById("spambox").innerHTML = tmp;
+		}, 100);
 	}
 })();
