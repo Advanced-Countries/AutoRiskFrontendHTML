a81641373d1fd8a121b5c08885ae0c30927479fe
zjjppiscool <jpzen@outlook.com>
2017-12-28 19:09:20 -0600

    less pingspam

--- autoriskbot.js
+++ autoriskbot.js
@@ -4,13 +4,13 @@
 		console.log('[CLIENT] Ready!');
 		window.generalChannel = client.guilds.find('id', '386688984845123585').channels.find('name', 'spam'); //Find the #spam channel, and set it to var generalChannel.
 		window.botDm = client.guilds.find('id', '386688984845123585').members.find("id", "386688418224275456"); //Find The Cartographer's user object, and save to botDm.
-		botDm.createDM();//Creates a dm if nonexistent.
-		botDm.send("!fullmap");//Gets the map, list of countries, and guns available.
-		botDm.send("!list")
-		botDm.send("!guns");
-		generalChannel.send("!stats " + client.user.toString());//Since there are no way to identify who sent these commands (lag, would havve to find the entire message list, etc) they are ran by mentioning the user.
-		generalChannel.send("!force " + client.user.toString());
-		generalChannel.send("!resource " + client.user.toString());
+		window.myId = client.user.id;
+		generalChannel.send("!fullmap");//Gets the map, list of countries, and guns available.
+		generalChannel.send("!list")
+		generalChannel.send("!guns");
+		generalChannel.send("!stats";
+		generalChannel.send("!force webInt");
+		generalChannel.send("!resource webInt");
 	});
 	client.on('debug', console.log);//Logs what the bot is doing.
 	client.on('error', console.error);
@@ -18,33 +18,26 @@
 	client.on('message', (message) => {//When it detects a message.
 		if (message.author.id === "386688418224275456") {//Only check messages if the author is the cartographer.
 			console.log(message.content);//Logs the message for simple debugging.
-			if (message.content === "") {//If the message is blank, it must have the map.
+			if (message.content === "Fullmap:") {//If the message is blank, it must have the map.
 				document.getElementById("map").src = message.attachments.first().url;//Grab the url of the map, and make it the src of the map image.
 			} else if (message.channel.id === "386688984845123587") {//If it is in the daily news chanenl, it is a daily news. So, put it in the daily news area.
 				document.getElementById("news").innerHTML = "Last Daily News:\n" + message.content;
-				botDm.send("!fullmap");
-				botDm.send("!list");
-				generalChannel.send("!stats " + client.user.toString());
-				generalChannel.send("!force " + client.user.toString());
-				generalChannel.send("!resource " + client.user.toString());
-			} else if (message.content.includes("guns")) {//If it includes the word guns, it is the gun list. Put it in the guns area.
+				generalChannel.send("!fullmap");
+				generalChannel.send("!list");
+				generalChannel.send("!stats");
+				generalChannel.send("!force webInt");
+				generalChannel.send("!resource webInt");
+			} else if (doesContain("guns",message)) {//If it includes the word guns, it is the gun list. Put it in the guns area.
 				document.getElementById("guns").innerHTML = message.content;
-			} else if (message.content.includes("force") & message.content.includes(client.user.toString().replace("<@", "<@!"))) {//If it contains force and your mention, it is your force. Put it in the force area.
-				document.getElementById("force").innerHTML = message.content;
-			} else if (message.content.includes("you mine") & message.content.includes(client.user.toString().replace("<@", "<@!"))) {//If it contains "you mine" and your mention, it is your resources. Put in in the resources ara.
-				document.getElementById("resource").innerHTML = message.content;
-			} else if (message.content.includes(client.user.tag)) {//If it contains your tag, ("<username><#1234>") it is your stats. Put it in the stats area.
+			} else if (doesContain(`${myId}WEBINT_FORCE`, message)) {//If it contains force and your id, it is your force. Put it in the force area.
+				document.getElementById("force").innerHTML = message.content.replace(`${myId}WEBINT_FORCE`, "");
+			} else if (doesContain(`${myId}WEBINT_RESOURCE`)) {//If it contains "you mine" and your mention, it is your resources. Put in in the resources ara.
+				document.getElementById("resource").innerHTML = message.content.replace(`${myId}WEBINT_RESOURCE`, "");
+			} else if (doesContain(client.user.tag, message)) {//If it contains your tag, ("<username><#1234>") it is your stats. Put it in the stats area.
 				document.getElementById("stats").innerHTML = "Your Stats:\n" + message.content;
-			} else if (message.content.includes("economyType:")) {//Someone else's stats.
-
-			} else if (message.content.includes("resource")) {//Someone else;'s resources.
-
-			} else if (message.content.includes("force")) {//Someone else's force.
-
-			} else if (message.content.includes("\n")) {//List of countries, so puut it in the list area.
-				document.getElementById("list").innerHTML = "List of Countries:\n" + message.content;
+			} else if (doesContain("List of countries:", message)){
+				document.getElementById("list").innerHTML = message.content;
 			}
-
 		}
 	});
 	client.login(localStorage.token || window.token || prompt('Please put your token here: (Open Discord, press ctrl-shift-i, go to Application, click local storage, and copy token.', 'abcdef123456').replace("\"", ""))
@@ -63,6 +56,9 @@
 			generalChannel.send("!resource " + client.user.toString());
 			generalChannel.send("!force " + client.user.toString());
 		}, 1000);
+	}
+	function doesContain(what,msg){
+		return msg.content.includes(what);
 	}
 	document.getElementById("warall").onclick = function () {
 		war("all");//Most of these are self explanatory, this one wars all.
